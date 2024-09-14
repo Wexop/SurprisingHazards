f7fe8ee8f9094d8cce65b3ba18850b411c4ab5e3
Wexop <mattheurtin@gmail.com>
2024-09-14 11:11:37 +0200

    ignore

--- obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SurprisingHazards
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "SurprisingHazards";
-        public const string PLUGIN_NAME = "SurprisingHazards";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
